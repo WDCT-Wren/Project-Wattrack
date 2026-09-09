5ea691d01df63f56044d6bde61cbd1c97bb98e07
WDCT-Wren <wrenctulio@gmail.com>
2026-09-09 21:58:38 +0800

    feat: implemented debounce function in pulse reading
    This impelmentation prevents the function from being triggered again right after being triggered 10 milliseconds ago. 10 miliseconds was chosen due to give headroom foraverage meter pulse time.

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,17 @@
 
 // define pin
 const uint8_t sensorPin = 2;  
+
 constexpr uint8_t PULSE_CONSTANT = 1600;
+const unsigned long DEBOUNCE_MICROS =  10000; // 0.01 seconds
 
 volatile unsigned long totalPulses = 0;
 volatile unsigned long kwh = 0;
+volatile unsigned long lastPulseTime = 0;
 
 unsigned long safeKwhRead = 0;
+
+
 
 void setup() {
   pinMode(sensorPin, INPUT); 
@@ -34,6 +39,10 @@ void loop() {
   reads and detects pulses of light from the photodiode
 */
 void readPulse() {
+  unsigned long now = micros();
+  if (now - lastPulseTime < DEBOUNCE_MICROS) return;
+  lastPulseTime = now; 
+
   totalPulses++;
   kwh = totalPulses / (float) PULSE_CONSTANT;
 }
