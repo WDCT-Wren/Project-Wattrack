4be01006611a0ec70515ecf00cf535c9b924bfc3
WDCT-Wren <wrenctulio@gmail.com>
2026-09-22 00:26:15 +0800

    refactor: streamline setup and loop functions by removing displayData and enhancing pulse count logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include "header.h"
 
 void setup() {
+  Serial.begin(115200);
+  initStorage();
   initSensor();
-  initDisplay();
   initLED();
+  initWebServer();
 }
 
 void loop() {
   pulseLED();
-  displayData();
+
+  Serial.print("Pulses counted: ");
+  Serial.println(totalPulses);
   
   delay(50);
 
   updateSafeKwh();
+  savePulseCount(totalPulses);
 }
